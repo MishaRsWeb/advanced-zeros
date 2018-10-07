955b0e960b952a977175e7cae5b5c105b60025e7
michael.rasskazov <misharasskazov1@gmail.com>
2018-10-07 20:52:37 +0400

    advanced-zero-finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+var zerosCount = Number.MAX_VALUE;
+var b = base;
+  for (var i = 2; i <= base; i++) {
+    if (b % i === 0) {
+var a = 0;
+      while (b % i === 0) {
+         b = b / i;     
+         a++;
+      }
+      var c = 0;
+      var z = Math.floor(number / i);
+      while (z > 0) {
+        c += z;
+        z = Math.floor(z / i);
+      }
+      zerosCount = Math.min(zerosCount, Math.floor(c / a))
+    }
+  }
+  return zerosCount;
 }
